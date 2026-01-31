be4fb798fdbf386f64449012c9ecc6ae0078ce4a
VirtualMan52 <virtu@virtualman52.com>
2026-01-31 15:38:58 -0500

    Random characters

---
+++ Assets/Scripts/CharacterLoader.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CharacterLoader : MonoBehaviour
+{
+    [SerializeField] private string loadType = "random";
+    public string colorID = "none";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (GetComponentInChildren<Animator>()) GetComponentInChildren<Animator>().Play("salsa" + Random.Range(1,6));
+        ResetCharacter();
+    }
+
+    public void ResetCharacter()
+    {
+        Renderer model = GetComponentInChildren<Renderer>();
+        if (loadType == "player") model.materials[0].SetTexture("_BaseMap",Resources.Load<Texture2D>("clothing/clothingtex_black"));
+        else if (loadType == "lover") model.materials[0].SetTexture("_BaseMap", Resources.Load<Texture2D>("clothing/clothingtex_white"));
+        else
+        {
+            List<Material> mats = new List<Material>();
+
+            model.materials[0].SetTexture("_BaseMap", Resources.Load<Texture2D>("clothing/clothingtex_" + colorID));
+
+            mats.Add(model.materials[0]);
+            mats.Add(Resources.Load<Material>("skin/skin" + Random.Range(1,7)));
+
+            model.SetMaterials(mats);
+        }
+    }
+}
