1f803c8816dbc4a0f04f6457929d99066e3f26c8
VirtualMan52 <virtu@virtualman52.com>
2026-01-30 20:12:44 -0500

    Slots and rings

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Slot
+{
+    public GameObject objAt;
+    public Vector3 pos;
+}
